02f3270e25cc44d6dbc1b0615ec3e4b327a316e9
Pabhat Mishra <135849019+CODxPRABHAT@users.noreply.github.com>
2025-03-22 15:06:11 +0530

    Update Target.jsx

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -6,7 +6,7 @@ import { useGSAP } from "@gsap/react";
 
 const Target = (props) => {
     const targetRef = useRef();
-    const {scene} = useGLTF('https:/vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf')
+    const {scene} = useGLTF('https://dbzylztppofeqeqcuooe.supabase.co/storage/v1/object/public/target-stand/model.gltf')
 
     useGSAP(() => {
             gsap.to(targetRef.current.position, {
